3edced28574aa91dbca68b0b1d0d2c30fb054ada
lty1904 <1835039863@qq.com>
2019-07-02 19:38:28 +0800

    修改了admin models settings 修改了外部配置，可以上传 db.sqlite3

--- demon1/booktest/admin.py
+++ demon1/booktest/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class HeroInfoInlines(admin.StackedInline):
+    model = HeroInfo
+    extra = 1
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ('title','pub_date')
+    list_filter = ('title','pub_date')
+    list_per_page = 10
+    inlines = [HeroInfoInlines]
+
+
+admin.site.register(BookInfo,BookInfoAdmin)
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['name','content']
+    search_fields = ['name','content']
+
+admin.site.register(HeroInfo,HeroInfoAdmin)
