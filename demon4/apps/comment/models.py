572587b141267052c36d734981c857f267c624e4
lty1904 <1835039863@qq.com>
2019-07-10 08:57:06 +0800

    demon4项目剩余未commit blog、comment、demon4

---
+++ demon4/apps/comment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from blog.models import *
+
+
+# Create your models here.
+
+class Comment(models.Model):
+    name=models.CharField(max_length=10)
+    url=models.URLField(blank=True,null=True,default='http://www.baidu.com')
+    email=models.EmailField(blank=True,null=True)
+    content=models.TextField()
+    create_time=models.DateTimeField(auto_now_add=True)
+    article=models.ForeignKey(Article,on_delete=models.CASCADE)
