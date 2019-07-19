da5c3fc4a1d59527a7e566bfd53fc6ab5ed9116c
lty1904 <1835039863@qq.com>
2019-07-19 11:46:46 +0800

    第三阶段项目 完善一下xadmin

--- part3_project/apps/comment/adminx.py
+++ part3_project/apps/comment/adminx.py
@@ -1,6 +1,7 @@
 import xadmin
 from .models import *
 
+class CommentAdminx(object):
+    list_display=['name','content','create_time','goods']
 
-
-xadmin.site.register(Comment)
+xadmin.site.register(Comment,CommentAdminx)
--- part3_project/apps/fruitmall/adminx.py
+++ part3_project/apps/fruitmall/adminx.py
@@ -3,15 +3,40 @@ from .models import *
 
 
 
+class AdsAdminx(object):
+    list_display=['img','desc','index']
 
+class AddGoodsAdminx(object):
+    list_display=['name','img','desc','price','num','wight','integral',
+                  'update_time','tags','category']
 
+class UserPackAdminx(object):
+    list_display=['address','integral','coupon','account']
 
-xadmin.site.register(Ads)
-xadmin.site.register(AddGoods)
-xadmin.site.register(UserPack)
-xadmin.site.register(UserAccount)
-xadmin.site.register(Tags)
-xadmin.site.register(Category)
-xadmin.site.register(Cart)
-xadmin.site.register(GoodsDetail)
-xadmin.site.register(News)
+class UserAccountAdminx(object):
+    list_display=['account','password','telephone','email']
+
+class CartAdminx(object):
+    list_display=['name','count']
+
+class GoodsDetailAdminx(object):
+    list_display=['name','sales','evaluate','img','body']
+
+class NewsAdminx(object):
+    list_display=['title','author','views','body','creatr_time']
+
+class TagsAdmin(object):
+    list_display=['title']
+
+class CategoryAdmins(object):
+    list_display=['title']
+
+xadmin.site.register(Ads,AdsAdminx)
+xadmin.site.register(AddGoods,AddGoodsAdminx)
+xadmin.site.register(UserPack,UserPackAdminx)
+xadmin.site.register(UserAccount,UserAccountAdminx)
+xadmin.site.register(Tags,TagsAdmin)
+xadmin.site.register(Category,CategoryAdmins)
+xadmin.site.register(Cart,CartAdminx)
+xadmin.site.register(GoodsDetail,GoodsDetailAdminx)
+xadmin.site.register(News,NewsAdminx)
